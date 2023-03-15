a63d559d743fd0eaf8020f547abde7d6f3fdd48b
chawsuuuhlaing22222 <minnaungyoo2015@gmail.com>
2023-03-15 20:00:10 +0630

    ticket confirm cmt6

--- app/src/main/java/com/padc/csh/themovieapplication/fragments/ProfileFragment.kt
+++ app/src/main/java/com/padc/csh/themovieapplication/fragments/ProfileFragment.kt
@@ -3,11 +3,10 @@ package com.padc.csh.themovieapplication.fragments
 import android.app.Dialog
 import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.Fragment
 import com.padc.csh.themovieapplication.R
 import com.padc.csh.themovieapplication.activities.LoginChooseActivity
 import kotlinx.android.synthetic.main.fragment_profile.*
@@ -37,9 +36,10 @@ class ProfileFragment : Fragment() {
             dialog?.setContentView(view)
             view.btnYes.setOnClickListener {
                 var intent=Intent(requireContext(),LoginChooseActivity::class.java)
-                intent.flags=Intent.FLAG_ACTIVITY_CLEAR_TOP
+                intent.addFlags( Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
                 startActivity(intent)
             }
+
             view.btnNo.setOnClickListener {
                 dialog?.dismiss()
             }
